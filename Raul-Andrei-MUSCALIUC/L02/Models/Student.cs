2199df2aa824025d78d6cd59210dbe500278ba39
raul.muscaliuc <raul.muscaliuc@dialogdata.de>
2020-10-04 21:08:53 +0300

    Rezolvare Tema 2

---
+++ Raul-Andrei-MUSCALIUC/L02/Models/Student.cs
@@ -0,0 +1,11 @@
+namespace L02.Models
+{
+    public class Student
+    {
+        public long Id { get; set;}
+        public string Nume { get; set;}
+        public string Prenume { get; set;}
+        public string Facultate  { get; set;}
+        public int AnStudiu { get; set;}
+    }
+}
---
+++ Raul-Andrei-MUSCALIUC/L02/Models/StudentContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace L02.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
